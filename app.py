446bb8d9b2d26df69c8cb82051183f863369f70a
andrewdempsey2018 <andrewdempsey2016@gmail.com>
2021-11-09 21:10:24 +0000

    refactor to a bog site

--- app.py
+++ app.py
@@ -4,23 +4,19 @@ from flask_pymongo import PyMongo
 import os
 
 app = Flask(__name__)
-app.config["MONGO_URI"] = "mongodb+srv://andrew2021:wildside1@andrewcluster.igjjx.mongodb.net/testdb?retryWrites=true&w=majority"
+app.config["MONGO_URI"] = "mongodb+srv://andrew2021:wildside1@andrewcluster.igjjx.mongodb.net/blogsdb?retryWrites=true&w=majority"
 mongo = PyMongo(app)
 
 #connect to the database using heroku env variable
-#app.config["MONGO_DBNAME"] = "IGF_DB"
 #app.config["MONGO_URI"] = os.getenv("MONGO_URI")
-#make an instance of PyMongo and pass the app in
 
-
-#load the home page along with a cursor containing all games currently in the database
 @app.route("/")
-def get_games():
-    return render_template("index.html")
+def index():
+    return render_template("index.html", blogs = mongo.db.blogscoll.find())
 
-@app.route("/text")
-def text():
-    return render_template("text.html", data = mongo.db.testcolls.find_one())
+@app.route("/create")
+def create():
+    return render_template("create.html")
 
 if __name__ == '__main__':
     app.run(debug=True)
